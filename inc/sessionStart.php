65974d2c234374e0ddeb7c552dac3c8ed0e85b3e
andresmchc456 <andresmchc456@gmail.com>
2025-06-20 13:39:29 -0500

    feat: guardar datos del usuario

---
+++ inc/sessionStart.php
@@ -0,0 +1,9 @@
+<!-- Esto te permite guardar datos del usuario mientras navega por tu sitio, como:
+nombre de usuario
+correo
+carrito de compras
+permisos -->
+<?php
+    session_name("INVENTARIO");
+    session_start();
+
--- index.php
+++ index.php
@@ -1,13 +1,16 @@
+<?php require "./inc/sessionStart.php" ; ?> 
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php include "./inc/head.php";?> 
+    <?php include "./inc/head.php";?> <!--ruta  elemento-->
 </head>
 <body>
     <?php
-        include "./inc/navbar.php";?>
+        include "./inc/navbar.php";?> <!--ruta  elemento-->
     <!-- Bootstrap Bundle JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    
 </body>
 </html>
