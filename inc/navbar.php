8b5d93e1d87767aa71429024b3a0935785435eb4
andresmchc456 <andresmchc456@gmail.com>
2025-06-20 12:43:21 -0500

    fead: intefaz

---
+++ inc/navbar.php
@@ -0,0 +1,23 @@
+<ul class="nav nav-underline d-flex w-100 align-items-center px-3">
+
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      Dropdown
+    </a>
+    <ul class="dropdown-menu">
+      <li><a class="dropdown-item" href="#">Opción 1</a></li>
+      <li><a class="dropdown-item" href="#">Opción 2</a></li>
+      <li><a class="dropdown-item disabled" href="#" tabindex="-1" aria-disabled="true">Opción deshabilitada</a></li>
+    </ul>
+  </li>
+
+  
+  <div class="d-flex ms-auto gap-3">
+    <li class="nav-item">
+      <a class="nav-link"  href="#">Active</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+  </div>
+</ul>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include "./inc/head.php";?> 
+</head>
+<body>
+    <?php
+        include "./inc/navbar.php";?>
+    <!-- Bootstrap Bundle JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
