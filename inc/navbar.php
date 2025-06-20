65857d9942688d9332f7b9e728eddfbccbe7a418
andresmchc456 <andresmchc456@gmail.com>
2025-06-20 13:48:56 -0500

    feat: correcion navbar

--- inc/navbar.php
+++ inc/navbar.php
@@ -2,26 +2,26 @@
   <div class="container-fluid">
     <!-- Título o logo -->
     <a class="navbar-brand" href="#">Inventario</a>
+
     <!-- Botón hamburguesa -->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContenido" aria-controls="navbarContenido" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    
-    
+
     <!-- Contenido que se colapsa -->
     <div class="collapse navbar-collapse" id="navbarContenido">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav nav-underline">
 
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-              Usuario
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Opción 1</a></li>
-              <li><a class="dropdown-item" href="#">Opción 2</a></li>
-              <li><a class="dropdown-item disabled" href="#">Opción deshabilitada</a></li>
-            </ul>
-          </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+            Usuario
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Opción 1</a></li>
+            <li><a class="dropdown-item" href="#">Opción 2</a></li>
+            <li><a class="dropdown-item disabled" href="#">Opción deshabilitada</a></li>
+          </ul>
+        </li>
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
@@ -43,17 +43,17 @@
             <li><a class="dropdown-item" href="#">Opción 2</a></li>
             <li><a class="dropdown-item disabled" href="#">Opción deshabilitada</a></li>
           </ul>
+        </li>
 
-           <!-- Sección derecha -->
-          <ul class="navbar-nav mb-2 mb-lg-0 ms-auto nav nav-underline">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Mi cuenta</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Salir</a>
-            </li>
-          </ul>
+      </ul>
 
+      <!-- Sección derecha -->
+      <ul class="navbar-nav mb-2 mb-lg-0 ms-auto nav nav-underline">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Mi cuenta</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Salir</a>
         </li>
       </ul>
     </div>
