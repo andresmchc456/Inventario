aa175f22dacafa385a17439ba4ae1b1a8aff1680
andresmchc456 <andresmchc456@gmail.com>
2025-06-20 13:39:57 -0500

    feat: responsivo de navbar

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,23 +1,61 @@
-<ul class="nav nav-underline d-flex w-100 align-items-center px-3">
+<nav class="navbar navbar-expand-lg navbar-light bg-light px-3">
+  <div class="container-fluid">
+    <!-- Título o logo -->
+    <a class="navbar-brand" href="#">Inventario</a>
+    <!-- Botón hamburguesa -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContenido" aria-controls="navbarContenido" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    
+    <!-- Contenido que se colapsa -->
+    <div class="collapse navbar-collapse" id="navbarContenido">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav nav-underline">
 
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-      Dropdown
-    </a>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="#">Opción 1</a></li>
-      <li><a class="dropdown-item" href="#">Opción 2</a></li>
-      <li><a class="dropdown-item disabled" href="#" tabindex="-1" aria-disabled="true">Opción deshabilitada</a></li>
-    </ul>
-  </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+              Usuario
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Opción 1</a></li>
+              <li><a class="dropdown-item" href="#">Opción 2</a></li>
+              <li><a class="dropdown-item disabled" href="#">Opción deshabilitada</a></li>
+            </ul>
+          </li>
 
-  
-  <div class="d-flex ms-auto gap-3">
-    <li class="nav-item">
-      <a class="nav-link"  href="#">Active</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
-    </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+            Categoría
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Opción 1</a></li>
+            <li><a class="dropdown-item" href="#">Opción 2</a></li>
+            <li><a class="dropdown-item disabled" href="#">Opción deshabilitada</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+            Producto
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Opción 1</a></li>
+            <li><a class="dropdown-item" href="#">Opción 2</a></li>
+            <li><a class="dropdown-item disabled" href="#">Opción deshabilitada</a></li>
+          </ul>
+
+           <!-- Sección derecha -->
+          <ul class="navbar-nav mb-2 mb-lg-0 ms-auto nav nav-underline">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Mi cuenta</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Salir</a>
+            </li>
+          </ul>
+
+        </li>
+      </ul>
+    </div>
   </div>
-</ul>
+</nav>
