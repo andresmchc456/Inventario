47a70867f42b67205c80d1e85c4231304e912b04
andresmchc456 <andresmchc456@gmail.com>
2025-07-08 11:27:45 -0500

    feat: se agrego la card en login

--- views/login.php
+++ views/login.php
@@ -1,26 +1,32 @@
-<div class="d-flex justify-content-center align-items-center vh-100">   
-    <div class="main-container" style="max-width: 1200px;">
-        <form class="row g-3">
+<div class="d-flex justify-content-center align-items-center vh-100 bg-light">   
+    <div class="card w-100 shadow" style="max-width: 500px;">
+        <div class="card-body">
+            <h2 class="card-title text-center mb-4">Sistema de Inventario</h2>
 
-            <div class="col-12">
-                <label for="inputEmail4" class="form-label">Email</label>
-                <input type="email" class="form-control" id="inputEmail4">
-            </div>
-            <div class="col-12">
-                <label for="inputPassword4" class="form-label">Password</label>
-                <input type="password" class="form-control" id="inputPassword4">
-            </div>
-            <div class="col-12">
-                <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck">
-                <label class="form-check-label" for="gridCheck">
-                    Check me out
-                </label>
+            <form class="row g-3">
+                <div class="col-12">
+                    <label for="inputEmail4" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4">
                 </div>
-            </div>
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary">Sign in</button>
-            </div>
-        </form>
+
+                <div class="col-12">
+                    <label for="inputPassword4" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="inputPassword4">
+                </div>
+
+                <div class="col-12">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="gridCheck">
+                        <label class="form-check-label" for="gridCheck">
+                            Recuérdame
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary w-100">Iniciar sesión</button>
+                </div>
+            </form>
+        </div>
     </div>
-</div>     
+</div>
