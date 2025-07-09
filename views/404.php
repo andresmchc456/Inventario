3ae8f67a617a7f09dbab9a4d4246593bd67aff52
andresmchc456 <andresmchc456@gmail.com>
2025-07-09 11:50:02 -0500

    estetica al 404

---
+++ views/404.php
@@ -0,0 +1,6 @@
+<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <div class="text-center">
+        <h2>Error 404:</h2>
+        <p>Página no encontrada</p>
+    </div>
+</div>
