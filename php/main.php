5edae187ec54c27a786c90aaddbbca8a3cb5dd7f
andresmchc456 <andresmchc456@gmail.com>
2025-07-15 15:28:34 -0500

    feat: se hizo inplemento el try/catch para la coptura de er rores

--- php/main.php
+++ php/main.php
@@ -1,12 +1,20 @@
 <!-- funciones principales 
 conexion base de datos  con extencio pdo-->
 <!-- instacia la clase PDO -->
+ <!-- http://localhost/Inventario/php/main.php -->
 <?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=inventario', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "✅ Conexión establecida correctamente";
-} catch (PDOException $e) {
-    echo "❌ Error en la conexión: " . $e->getMessage();
+function conexion(){
+    try {
+    $pdo = new PDO('mysql:host=localhost;dbname=inventario', 'root', '');// Cambia 'root' y '' por tus credenciales de base de datos
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);// Configurar el modo de error para lanzar excepciones
+    echo "✅ Conexión establecida correctamente<br>";
+    return $pdo; // Retorna la instancia de PDO para su uso posterior
+    } catch (PDOException $e) {// Capturar errores de conexión
+        echo "❌ Error en la conexión: " . $e->getMessage();
+        exit(); // detener el script si falla la conexión
+    }
 }
+
+// peticion o insercion en la base de datos en la tabla categoria
+// $pdo->query("INSERT INTO categoria(categoria_nombre, categoria_ubicacion) VALUES('prueba', 'texto ubicacion')");
 ?>
