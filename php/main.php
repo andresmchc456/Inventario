7f9d8f6e5acaf203d42c1a84069f5657e43aad9f
Andrés Meléndez <andresmchc456@gmail.com>
2025-07-29 10:55:10 -0500

    feat: lipiar cadenas de texto y funcion renombrar fotos

--- php/main.php
+++ php/main.php
@@ -31,4 +31,52 @@ $pdo = conexion();
      }
  }
 
+ #lipiar cadenas de texto
+ function limpiar_cadena($cadena){
+    $cadena = trim($cadena); // Elimina espacios al inicio y al final
+    $cadena  = stripslashes($cadena); // Elimina barras invertidas
+    $cadena = str_ireplace("<script>", "", $cadena);// Elimina caracteres especiales
+    $cadena = str_ireplace("</script>", "", $cadena);
+    $cadena=str_ireplace("<script src", "", $cadena);
+    $cadena=str_ireplace("<script type=", "", $cadena);
+    $cadena=str_ireplace("SELECT * FROM", "", $cadena);
+    $cadena=str_ireplace("DELETE FROM", "", $cadena);
+    $cadena=str_ireplace("INSERT INTO", "", $cadena);
+    $cadena=str_ireplace("DROP TABLE", "", $cadena);
+    $cadena=str_ireplace("DROP DATABASE", "", $cadena);
+    $cadena=str_ireplace("TRUNCATE TABLE", "", $cadena);
+    $cadena=str_ireplace("SHOW TABLES;", "", $cadena);
+    $cadena=str_ireplace("SHOW DATABASES;", "", $cadena);
+    $cadena=str_ireplace("<?php", "", $cadena);
+    $cadena=str_ireplace("?>", "", $cadena);
+    $cadena=str_ireplace("--", "", $cadena);
+    $cadena=str_ireplace("^", "", $cadena);
+    $cadena=str_ireplace("<", "", $cadena);
+    $cadena=str_ireplace("[", "", $cadena);
+    $cadena=str_ireplace("]", "", $cadena);
+    $cadena=str_ireplace("==", "", $cadena);
+    $cadena=str_ireplace(";", "", $cadena);
+    $cadena=str_ireplace("::", "", $cadena);
+    $cadena=trim($cadena);
+    $cadena=stripslashes($cadena);
+    return $cadena;
+ }
+//  $texto="<script>  Hola mundo </script>";
+//  echo limpiar_cadena($texto);
+
+#funcion renombrar fotos
+ function renombrar_fotos($nombre){
+    $nombre=str_ireplace(" ", "_", $nombre);
+    $nombre=str_ireplace("/", "_", $nombre);
+    $nombre=str_ireplace("#", "_", $nombre);
+    $nombre=str_ireplace("-", "_", $nombre);
+    $nombre=str_ireplace("$", "_", $nombre);
+    $nombre=str_ireplace(".", "_", $nombre);
+    $nombre=str_ireplace(",", "_", $nombre);
+    $nombre=$nombre."_".rand(0,100);// Agrega un número aleatorio al final del nombre
+    return $nombre;
+ }
+
+ $foto="Play Station 5 Black/edition";
+ echo renombrar_fotos($foto); // Muestra el nombre de la foto renombrado
 ?>
