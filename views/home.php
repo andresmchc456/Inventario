e4602819c2b742ee19d8247606f9b3a107ae29f2
Andrés Meléndez <andresmchc456@gmail.com>
2025-09-10 12:32:35 -0500

    feat: se elimino comentarios

--- views/home.php
+++ views/home.php
@@ -1,14 +1,4 @@
 
-
-<!-- <nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination"> //esta parte de la etiqueta  se pego en la funcion  function paginador_tablas en el archivo main -->
-  <!-- <ul class="pagination"> -->
-    <!-- <li class="page-item"><a class="page-link" href="#">Anterior</a></li> se llevo a la funcion paginador-->
-    <!-- <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>  -->
-    <!-- <li class="page-item"><a class="page-link" href="#">Siguiente</a></li>
-  </ul> -->
-<!-- </nav> // aca se cerro el la funcion paginador-->
 <div>
     <h1>Home</h1>
     <h2>Bienvenido  <?php echo $_SESSION['nombre']. " ". 
